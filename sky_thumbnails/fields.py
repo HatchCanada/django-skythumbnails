90254c5498afbe9fab76f7c91d0aec0b8fe76b4c
Neal Striegler-Pettersson <strigs@gmail.com>
2019-10-18 11:53:49 -0400

    remove redundant check, bump version

--- sky_thumbnails/fields.py
+++ sky_thumbnails/fields.py
@@ -74,7 +74,7 @@ class BaseThumbnailFieldFile(ImageFieldFile):
         super(BaseThumbnailFieldFile, self).__init__(instance, field, name)
 
     def get_identifier(self, identifier):
-        if not isinstance(identifier, str) and not isinstance(identifier, str):
+        if not isinstance(identifier, str):
             raise ThumbnailOptionError('The identifier must be a string')
         elif identifier == '':
             raise ThumbnailOptionError('An identifier (key) for the thumbnails dictionary was blank')
